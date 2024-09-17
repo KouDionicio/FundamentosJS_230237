1847e14c638a523fb715acf3a4b3ebe8ebe7baad
Citlalli Perez Dionicio <230237@utxicotepec.edu.mx>
2024-09-17 15:22:31 -0600

    Pràctica 4-Ejercicio 3
    Agregaciòn de los Tipos de Datos String, Big Init y Symbol

---
+++ JS/Ejercicio_03.js
@@ -0,0 +1,37 @@
+//String o Cadenas de texto
+
+const alumno = "Juan";
+let producto = 'Monitor';
+
+//const numero = 30;
+//const numero2 = 20;
+
+//console.log(typeof numero);
+//console.log(typeof numero2);
+
+// const numeroGrande = BigInt(918298193891887139831091038173813);
+// console.log(typeof numeroGrande)
+ 
+// const numero = 10;
+// const numero2 = 20;
+// console.log(numero + Number(numeroGrande));
+
+//Para ocupar una operacion de una cadena de texto con un numero
+// const numero = "30";
+// const numero2 = 20;
+
+// console.log(typeof String(numero2));
+// console.log(typeof Number(numero));
+
+
+//Symbol
+// const primerSymbol = Symbol(30);
+// const segundoSymbol = Symbol(30);
+// console.log(primerSymbol === segundoSymbol);
+// console.log(primerSymbol.valueOf());
+
+
+
+//Null
+const descuento = null;
+console.log(typeof descuento);
